99a97a461627f4a34e66a698f03af943dc40150c
KaiqueMDEV <kaiquemedeirosmoreira@gmail.com>
2024-10-02 22:19:25 -0300

    terminado adivinhação
    falta adicionar ao projeto

--- adivinhação/script.js
+++ adivinhação/script.js
@@ -2,7 +2,20 @@ function checar(){
     x = parseInt(document.getElementById("number").value)
     if (x == 61){
         document.getElementById("resposta").style.backgroundColor = "green";
-        document.get
+        document.getElementById("resposta").style.height = "auto"
+        document.getElementById("resposta").innerHTML = "Correto!";
 
-    } 
+    } else if (x < 61) {
+        document.getElementById("resposta").style.backgroundColor = "red";
+        document.getElementById("resposta").innerHTML = "Maior!";
+    } else if (x > 61) {
+        document.getElementById("resposta").style.backgroundColor = "red";
+        document.getElementById("resposta").innerHTML = "Menor!";
+    }
+} 
+
+function gerar(){
+    num = Math.floor(Math.random() * 99) + 1;
+    document.getElementById("number").value = num;
 }
+
