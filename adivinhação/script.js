802851bbbb9e402bd887319df72306f92559571c
KaiqueMDEV <kaiquemedeirosmoreira@gmail.com>
2024-10-14 19:24:28 -0300

    adivinhação corrigido

--- adivinhação/script.js
+++ adivinhação/script.js
@@ -1,14 +1,16 @@
+num = Math.floor(Math.random() * 99) + 1;
+
 function checar(){
     x = parseInt(document.getElementById("number").value)
-    if (x == 61){
+    if (x == num){
         document.getElementById("resposta").style.backgroundColor = "green";
         document.getElementById("resposta").style.height = "auto"
         document.getElementById("resposta").innerHTML = "Correto!";
 
-    } else if (x < 61) {
+    } else if (x < num) {
         document.getElementById("resposta").style.backgroundColor = "red";
         document.getElementById("resposta").innerHTML = "Maior!";
-    } else if (x > 61) {
+    } else if (x > num) {
         document.getElementById("resposta").style.backgroundColor = "red";
         document.getElementById("resposta").innerHTML = "Menor!";
     }
@@ -16,6 +18,5 @@ function checar(){
 
 function gerar(){
     num = Math.floor(Math.random() * 99) + 1;
-    document.getElementById("number").value = num;
 }
 
